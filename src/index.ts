366d6c79fdd5b8ed68155ec97145190fadd3c4f1
Alex Scotton <alex@alexscotton.com>
2023-01-02 12:48:06 +0000

    Adds a basic scheduled worker

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,70 @@
-type Env = {}
+import { isAfter, subDays } from "date-fns";
+import { Job, JobBoard } from "./lib/jobs";
+
+type Env = {
+	AIRTABLE_API_TOKEN: string;
+	DISCORD_API_TOKEN: string;
+	DISCORD_CHANNEL: string;
+};
+
+type Locations = Record<string, Job[]>;
 
 export default {
-	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext) {
-		// [...]
+	async scheduled(
+		event: ScheduledEvent,
+		{ AIRTABLE_API_TOKEN, DISCORD_API_TOKEN, DISCORD_CHANNEL }: Env,
+		ctx: ExecutionContext
+	) {
+		// Get Jobs List from the Provider
+		const provider = new JobBoard(AIRTABLE_API_TOKEN);
+		const jobs = await provider.get();
+
+		// Filter for Jobs created in the last week
+		const after = subDays(new Date(), 7);
+		const latestJobs = jobs.filter(job => isAfter(job.createdTime, after));
+
+		// Check we have any results
+		if (latestJobs.length === 0) return;
+
+		// Group the jobs in to Locations
+		const locations = latestJobs.reduce((locations, job) => {
+			if (!locations[job.Location]) {
+				locations[job.Location] = [];
+			}
+
+			locations[job.Location].push(job);
+
+			return locations;
+		}, {} as Locations);
+
+		// Create the message content
+		const header = "Your weekly dose of job role goodness:";
+		const body = Object.keys(locations)
+			.map(Location => [
+				createJobLocationHeader(Location),
+				...locations[Location].map(createJobListing),
+			])
+			.flat();
+		const content = [header, ...body].join("\n");
+
+		// Send it to Discord
+		await fetch(`https://discord.com/api/v10/channels/${DISCORD_CHANNEL}/messages`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"Authorization": `Bot ${DISCORD_API_TOKEN}`,
+			},
+			body: JSON.stringify({
+				content,
+			}),
+		});
 	},
+};
+
+const createJobLocationHeader = (Location: string): string => `\n## ${Location}:\n`;
+
+const createJobListing = ({ Title, Salary, LocationType, Url }: Job): string => {
+	const LocationTypeString = LocationType.join(" | ");
+
+	return `:link: **${Title}:** @ ${Salary} [${LocationTypeString}] <${Url}>`;
 };
---
+++ src/lib/jobs.ts
@@ -0,0 +1,52 @@
+const BASE_URL = "https://api.airtable.com/v0/appgLXjsOyliS6ndz";
+const TABLE = "Jobs";
+
+type AirtableRecord = {
+	id: string;
+	createdTime: string;
+	fields: any;
+};
+
+export type Job = {
+	id: string;
+	createdTime: Date;
+	Title: string;
+	Salary: string;
+	Location: string;
+	LocationType: LocationType[];
+	Url: string;
+};
+
+type LocationType = "Hybrid" | "Remote" | "On Site";
+
+export class JobBoard {
+	#apiKey;
+
+	constructor(apiKey: string) {
+		this.#apiKey = apiKey;
+	}
+
+	async get(): Promise<Job[]> {
+		const response = await this.#request();
+		const json: { records: AirtableRecord[] } = await response.json();
+
+		const jobs = json.records.map(({ id, createdTime, fields }) => ({
+			...fields,
+			id,
+			createdTime: new Date(createdTime),
+		}));
+
+		return jobs;
+	}
+
+	async #request() {
+		const url = `${BASE_URL}/${TABLE}`;
+
+		return fetch(url, {
+			headers: {
+				"Content-Type": "application/json",
+				"Authorization": `Bearer ${this.#apiKey}`,
+			},
+		});
+	}
+}
